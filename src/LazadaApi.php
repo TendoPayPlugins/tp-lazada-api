839cc47e41479bb7d43d35b8bff6ce826be3596a
mmichalak87 <73780271+mmichalak87@users.noreply.github.com>
2025-10-14 15:44:36 +0200

    Cast config values to string in LazadaApi constructor

--- src/LazadaApi.php
+++ src/LazadaApi.php
@@ -14,8 +14,8 @@ final class LazadaApi
 
     public function __construct()
     {
-        $this->appKey = config('lazada.api_key', '');
-        $this->appSecret = config('lazada.api_secret', '');
-        $this->callbackUrl = config('lazada.callback_url', '');
+        $this->appKey = (string) (config('lazada.api_key', '') ?? '');
+        $this->appSecret = (string) (config('lazada.api_secret', '') ?? '');
+        $this->callbackUrl = (string) (config('lazada.callback_url', '') ?? '');
     }
 }
